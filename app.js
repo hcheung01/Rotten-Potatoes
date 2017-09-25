c1a85824065cb3395fb3c90d2c4758d4b776f9e0
Heindrick <hcheung01@gmail.com>
2017-09-25 16:17:38 -0700

    init

--- app.js
+++ app.js
@@ -1,75 +1,78 @@
 var express = require('express')
+var exphbs = require('express-handlebars')
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
 var methodOverride = require('method-override')
 var app = express()
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes', { useMongoClient: true, })
+// add body-parser to app (after initializing above)
+app.use(bodyParser.urlencoded({ extended: true }))
 // override with POST having ?_method=DELETE or ?_method=PUT
 app.use(methodOverride('_method'))
 
-var exphbs  = require('express-handlebars');
-var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes');
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
 var Review = mongoose.model('Review', {
-  title: String,
-  description: String,
-  movieTitle: String
-});
-
-mongoose.connect('mongodb://localhost/rotten-potatoes');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-
-
-app.listen(3000, function () {
-  console.log('Portfolio App listening on port 3000!')
+    title: String,
+    movieTitle: String,
+    rating: Number,
+    description: String
 })
 
-// INDEX
-app.get('/', function (req, res) {
-  Review.find(function(err, reviews) {
-    res.render('reviews-index', {reviews: reviews});
-  })
+//GET review index
+app.get('/', function (req,res) {
+    Review.find(function(err, reviews) {
+        if (err) {
+            console.log(err)
+            return
+        }
+        res.render('reviews-index', {reviews: reviews})
+    })
 })
 
-// NEW
+//GET new index form
 app.get('/reviews/new', function (req, res) {
-  res.render('reviews-new', {});
+    res.render('reviews-new', {})
 })
 
-// CREATE
-app.post('/reviews', function (req, res) {
-  Review.create(req.body, function(err, review) {
-    res.redirect('/reviews/' + review._id);
-  })
-})
-
-// SHOW
+//GET specific review
 app.get('/reviews/:id', function (req, res) {
-  Review.findById(req.params.id).exec(function (err, review) {
-    res.render('reviews-show', {review: review});
-  })
-});
+    Review.findById(req.params.id).exec(function (err, review) {
+        res.render('reviews-show', {review: review})
+    })
+})
 
-// EDIT
+//GET specific review edit form
 app.get('/reviews/:id/edit', function (req, res) {
-  Review.findById(req.params.id, function(err, review) {
-    res.render('reviews-edit', {review: review});
-  })
+    Review.findById(req.params.id, function (err, review) {
+        res.render('reviews-edit', {review: review})
+    })
 })
 
-// UPDATE
+//POST(create) new review
+app.post('/reviews', function (req, res) {
+    Review.create(req.body, function(err, review) {
+        console.log(review)
+
+        res.redirect('/reviews/' + review._id)
+    })
+})
+
+//PUT(update) specific review
 app.put('/reviews/:id', function (req, res) {
-  Review.findByIdAndUpdate(req.params.id,  req.body, function(err, review) {
-    res.redirect('/reviews/' + review._id);
-  })
+    Review.findByIdAndUpdate(req.params.id, req.body, function(err, review) {
+        res.redirect('/reviews/' + review._id)
+    })
 })
 
-// DELETE
+//DELETE specific review
 app.delete('/reviews/:id', function (req, res) {
-  Review.findByIdAndRemove(req.params.id, function(err) {
-    res.redirect('/');
-  })
+    Review.findByIdAndRemove(req.params.id, function(err) {
+        res.redirect('/')
+    })
+})
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Portfolio App listening on port 3000!')
 })
